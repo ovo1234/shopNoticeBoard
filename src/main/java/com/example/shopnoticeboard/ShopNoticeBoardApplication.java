653d1b3342eb458f21db12446584a4db0b84ca9e
DESKTOP-M3HIP2O\dbwjd <dbwjd20111@naver.com>
2023-03-31 02:02:23 +0900

    시작

---
+++ src/main/java/com/example/shopnoticeboard/ShopNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopNoticeBoardApplication.class, args);
+    }
+
+}
